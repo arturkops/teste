6f7298697e37aa4a962249d092b2cb436d6446a6
arturkops <60902641+arturkops@users.noreply.github.com>
2022-10-12 17:08:30 -0300

    enviar

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<h1>Yappa Logs - The Quickest Analyser</h1> 
+<h1>Cu de pessego - The Quickest Analyser</h1> 
 <form enctype="multipart/form-data" action="upload.php" method="post">
       <!-- <input type="hidden" name="MAX_FILE_SIZE" value="300000" /> -->
       <p><input name="userfile" type="file" onChange="form.submit()" /></p>
